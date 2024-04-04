7673a11054f0a374a60d0b924e05ba9929f5fc3b
Magnus Quist <cloudquist@gmail.com>
2024-04-04 16:21:05 +0200

    initial commit

---
+++ src/test/java/dk/sdu/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.sdu.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
